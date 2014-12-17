88bf28b16a22b3f2a8b7d2f18d8936ae1028522e
Avijit Gupta <526avijitgupta@gmail.com>
2014-12-17 05:31:41 +0530

    conversion to pep8, additional enhancements

--- .post.py
+++ .post.py
@@ -1,55 +1,71 @@
 from urllib import urlencode
 from urllib2 import urlopen
-import os
-import subprocess, signal
-import sys
+from os import system, devnull
+import subprocess
+import signal
+from sys import argv, exit
 from time import sleep
 import httplib
+from json import load
 from check import *
-import json
 
 # For periodic checking of login status
 SLEEP_TIME = 200
 
 BASE_URL = "http://172.16.68.6:8090/login.xml"
 
-user_dict = json.load(open('credentials.json'))
+try:
+    user_dict = load(open('credentials.json'))
+except ValueError:
+    print """Error decoding the JSON file.
+    [Note: Check that there is a trailing comma after every line in the
+    credentials.json file, except the last one].
+    """
+    exit()
+
 
 # Function to send login or logout request
 def send_request(request_type, *arg):
     if(request_type == 'login'):
         print "Initialting login request for USERNAME: %s" % arg[0]
-        params = urlencode({'mode':191, 'username':arg[0], 'password':arg[1]})
+        params = urlencode(
+            {'mode': 191, 'username': arg[0], 'password': arg[1]})
     elif(request_type == 'logout'):
         print "Initiating logout request.."
-        params = urlencode({'mode':193, 'username':arg[0]})
+        params = urlencode({'mode': 193, 'username': arg[0]})
 
     response = urlopen(BASE_URL, params)
     return response.read()
 
+
+# Function to alert the user on successful login/logout
 def notify_alert(string):
     print string
-    os.system('notify-send ' + '"' + string + '"')
+    system('notify-send ' + '"' + string + '"')
 
+
+# Function to initiate a logout request and kill the ongoing login request
 def logout():
     for username in user_dict:
-        data = send_request('logout',username)
+        data = send_request('logout', username)
         if 'off' in data:
             notify_alert("Successfully logged off")
-            os.system("ps aux | grep python | grep '.post.py login' | awk '{print $2}' | xargs kill -9")
+            system(
+                """ps aux | grep python | grep '.post.py login' |
+                awk '{print $2}' | xargs kill -9""")
             break
 
-    
+
 if __name__ == "__main__":
 
     login_check = False
-    
+
     try:
-        if "login" in sys.argv:
+        if "login" in argv:
             for username in user_dict:
                 username = str(username)
                 user_dict[username] = str(user_dict[username])
-                data = send_request("login", username, user_dict[username]) 
+                data = send_request("login", username, user_dict[username])
                 if "Maximum" in data:
                     print "Maximum Login Limit Reached"
                 elif "exceeded" in data:
@@ -57,12 +73,15 @@ if __name__ == "__main__":
                 elif "could not" in data:
                     print "Incorrect username or password"
                 elif "into" in data:
-                    notify_alert("Successfully logged in with {0} ".format(username))
+                    notify_alert(
+                        "Successfully logged in with {0} ".format(username))
                     login_check = True
                     launch_chrome = True
                     try:
-                        ps = subprocess.Popen(('ps', 'aux'), stdout=subprocess.PIPE)
-                        output = subprocess.check_output(('pgrep', 'chrome'), stdin=ps.stdout)
+                        ps = subprocess.Popen(
+                            ('ps', 'aux'), stdout=subprocess.PIPE)
+                        output = subprocess.check_output(
+                            ('pgrep', 'chrome'), stdin=ps.stdout)
                         output = output.split("\n")
                         if (len(output) > 2):
                             launch_chrome = False
@@ -71,13 +90,18 @@ if __name__ == "__main__":
                     except Exception as e:
                         print e
 
-                    # Uncomment the following piece of code for automatic launching of google chrome(if not already open) on login
-                        
-                    # if launch_chrome:
-                        # print "Launching chrome.."
-                        # with open(os.devnull, 'wb') as devnull:
-                        #    subprocess.Popen('google-chrome', stdout=devnull, stderr=subprocess.STDOUT)
-                        
+                    """Uncomment this piece of code to
+                    automatically launch google chrome after login:
+                    [Note: google-chrome will be launched only if
+                    it is not already open, and not otherwise]"""
+
+                    """if launch_chrome:
+                       print "Launching chrome.."
+                       with open(devnull, 'wb') as devnull:
+                          subprocess.Popen(
+                          'google-chrome', stdout=devnull,
+                          stderr=subprocess.STDOUT)"""
+
                     print "Press Ctrl + C to logout"
                     break
                 else:
@@ -93,10 +117,11 @@ if __name__ == "__main__":
         logout()
         sys.exit()
     except httplib.BadStatusLine:
-        notify_alert("Bad Connection. Please check your connection and try again.")
+        notify_alert(
+            "Bad Connection. Please check your connection and try again.")
         sys.exit()
     except Exception as e:
         print e
-        
-    if "logout" in sys.argv:
+
+    if "logout" in argv:
         logout()
