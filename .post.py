6255efe3c368c4318c61da6f61cd68c36ed690eb
Tanmay Datta <tanmaydatta@gmail.com>
2015-04-13 11:06:05 +0530

    fixed directory path of credentials file in .post.py

--- .post.py
+++ .post.py
@@ -1,6 +1,7 @@
 from urllib import urlencode
 from urllib2 import urlopen
 from os import system, devnull
+import os
 import subprocess
 import signal
 from sys import argv, exit
@@ -17,7 +18,8 @@ BASE_URL = "http://172.16.68.6:8090/login.xml"
 BROWSERS_DICT = {"0": "", "1": "firefox", "2": "google-chrome"}
 
 try:
-    credentials = load(open('.credentials.json'))
+    jsonPath = os.path.dirname(os.path.realpath(__file__))
+    credentials = load(open(jsonPath + '/.credentials.json'))
     user_dict = credentials[0]
     browser = BROWSERS_DICT[credentials[1]["web-browser"]]
     print browser
